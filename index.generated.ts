b046aa0665152af13326c8abd411c4686ee5e0b5
TOPKAT <webmaster@nicewebagence.com>
2026-02-18 16:52:19 +0100

    ✨ NEW RecursiveRequired type helper

--- index.generated.ts
+++ index.generated.ts
@@ -34,6 +34,14 @@ export type RecursivePartial<T> = {
   : T[P];
 }
 
+export type RecursiveRequired<T> = {
+  [P in keyof T]-?: T[P] extends (infer U)[]
+  ? RecursiveRequired<U>[]
+  : T[P] extends object
+  ? RecursiveRequired<T[P]>
+  : T[P];
+}
+
 export type Letters = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
 
 export type SimpleNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
